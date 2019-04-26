b0e57a6bdd0515c3eee7712ef7594ef857674385
Parker <parker.hampson@gmail.com>
2019-04-26 16:57:25 -0700

    finished Contact Us tests, and then put the code into multiple steps

--- tests/navigate_to_site.js
+++ tests/navigate_to_site.js
@@ -1,15 +1,27 @@
 const globals = require('./settings/globals.js');
 
 module.exports = {
-  'Navigates To The EKA Site': (browser) => {
+  'Navigates to the EKA site, and displays logo': (browser) => {
     return browser
       .url(globals.url)
       .waitForElementVisible('.nav-logo')
+
+  },
+
+  'NavBar links are visable': (browser) => {
+    return browser
       .waitForElementVisible('.solutions-link')
       .waitForElementVisible('.services-link')
       .waitForElementVisible('.company-link')
       .waitForElementVisible('.contactus-btn-link')
       .waitForElementVisible('.login-link')
+  },
+  
+  'Contact Us displays proper modal when clicked': (browser) => { 
+    return browser
+      .click('#nav-contact-us')
+      .waitForElementVisible('.m-top-60')
+      .assert.containsText('.m-top-60', 'HOW CAN WE HELP?')
       .end();
   }
 }
