2b532e8e01b2757b5bd50e36490d9f81bf30f40a
Parker <parker.hampson@gmail.com>
2019-04-26 22:36:45 -0700

    Mobile navbar is properly tested now

--- tests/mobile_tests.js
+++ tests/mobile_tests.js
@@ -6,6 +6,16 @@ module.exports = {
       .url(globals.url)
       .resizeWindow(750, 1300)
       .waitForElementVisible('.nav-logo')
+  },
+
+  'Collapsed NavBar links are visable for mobile': (browser) => {
+    return browser
+      .click('.navbar-toggle')
+      .waitForElementVisible('.solutions-link')
+      .waitForElementVisible('.services-link')
+      .waitForElementVisible('.company-link')
+      .waitForElementVisible('.contactUs-link')
+      .waitForElementVisible('.login-link')
       .end();
   }
 }
